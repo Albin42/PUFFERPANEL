5dfba0a3ec4830decfab5d09631574a6a4ea1b8e
Albin42 <105271455+Albin42@users.noreply.github.com>
2024-04-03 07:13:43 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2eZJouK96uAAIA9b9hcDj5pkEox_2VtWf29R3AZZiJftjM3hj #Put Yours here
+./ngrok config add-authtoken 2eZOOE70A1fccVjfIbp6vs0Cy7h_52k829CHrSAnJHEwPshV7 #Put Yours here
 ./ngrok http 8080
